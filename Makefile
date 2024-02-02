380f9828cccc099f4de9aa3e3f3c281d856697ff
u8slvn <u8slvn@gmail.com>
2024-02-03 00:04:28 +0100

    feat: add split word and base structure

---
+++ Makefile
@@ -0,0 +1,21 @@
+.DEFAULT_GOAL := help
+
+.PHONY: help
+help: ## List all the command helps.
+	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+
+.PHONY: init-pre-commit
+init-pre-commit: ## Init pre-commit.
+	@pre-commit install
+	@pre-commit install --hook-type commit-msg
+
+.PHONY: lint
+lint: ## Check linter.
+	@pre-commit run --all-files
+
+.PHONY: test
+test: ## Run test.
+	@cargo test
+
+.PHONY: ci
+ci: lint test ## Run CI.
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,43 @@
 use pyo3::prelude::*;
+use rayon::prelude::*;
+use regex::Regex;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+const RE_WORDS: &str = r"\w+|[^\w]|[\s]";
+
+// fn load_regexen() -> HashMap<&str, Regex> {
+//     let mut regexen = HashMap::new();
+//     regexen.insert("words",  Regex::new(RE_WORDS).unwrap());
+//     Ok(regexen)
+// }
+
+// Extract all words from the given text
+fn split_words(text: &str) -> Vec<&str> {
+    let re_words = Regex::new(RE_WORDS).unwrap();
+    re_words.find_iter(text).map(|m| m.as_str()).collect()
 }
 
-/// A Python module implemented in Rust.
+fn process_word(word: &str, affix: &str, postfix: &str) -> String {
+    let mid_word = word.len() / 2;
+    let start = &word[..mid_word];
+    let end = &word[mid_word..];
+
+    format!("{}{}{}{}", affix, start, postfix, end)
+}
+
+#[pyfunction]
+fn write(text: &str, affix: &str, postfix: &str) -> PyResult<String> {
+    let words: Vec<_> = split_words(text);
+    let processed_words: Vec<_> = words
+        .par_iter()
+        .map(|word| process_word(word, affix, postfix))
+        .collect();
+
+    Ok(processed_words.join(""))
+}
+
 #[pymodule]
 fn bionic_writer(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(write, m)?)?;
     Ok(())
 }
 
@@ -18,9 +46,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn internal() {
-        let result = sum_as_string(2, 2);
-        let expected = String::from("4");
-        assert_eq!(expected, result.unwrap());
+    fn test_split_words() {
+        let text = "Your bones don't break, mine do. That's clear.";
+
+        let result: Vec<_> = split_words(text);
+
+        let expected: usize = 22;
+        assert_eq!(expected, result.len());
     }
 }
