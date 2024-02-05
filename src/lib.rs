7aaade227d1b9f30dad92cdbe7a3d5993d7b6da4
u8slvn <u8slvn@gmail.com>
2024-02-05 09:52:41 +0100

    fix: number regex use and handle utf8 slice

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,14 @@ use std::collections::HashMap;
 
 #[macro_use]
 extern crate lazy_static;
+extern crate utf8_slice;
 
 lazy_static! {
     static ref REGEXEN: HashMap<&'static str, Regex> = {
         let mut m = HashMap::new();
         m.insert("WORDS", Regex::new(r"\w+|\W|\s").unwrap());
         m.insert("WHITESPACE", Regex::new(r"^\s$").unwrap());
-        m.insert("NUMBER", Regex::new(r"^\d$").unwrap());
+        m.insert("NUMBER", Regex::new(r"^\d+$").unwrap());
         m.insert("NON_WORD", Regex::new(r"^\W$").unwrap());
         m
     };
@@ -25,23 +26,21 @@ fn split_words(text: &str) -> Vec<&str> {
 
 // Format word to bionic reading style.
 fn format_word(word: &str, affix: &str, postfix: &str, index: usize) -> String {
-    let start = &word[..index];
-    let end = &word[index..];
+    let start = utf8_slice::till(word, index);
+    let end = utf8_slice::from(word, index);
     format!("{}{}{}{}", affix, start, postfix, end)
 }
 
 fn process_word(word: &str, affix: &str, postfix: &str) -> String {
     if REGEXEN.get("WHITESPACE").unwrap().is_match(word)
         || REGEXEN.get("NON_WORD").unwrap().is_match(word)
+        || REGEXEN.get("NUMBER").unwrap().is_match(word)
     {
         return word.to_string();
     }
 
-    if REGEXEN.get("NUMBER").unwrap().is_match(word) {
-        return format_word(word, affix, postfix, word.len());
-    }
-
-    let index = word.len() / 2;
+    let word_len = utf8_slice::len(word);
+    let index = if word_len == 1 { 1 } else { word_len / 2 };
     format_word(word, affix, postfix, index)
 }
 
@@ -79,13 +78,38 @@ mod tests {
     #[test]
     fn test_format_word() {
         let word = "Hello";
-        let affix = "<b>";
-        let postfix = "</b>";
+        let affix = "[b]";
+        let postfix = "[/b]";
         let index = 2;
 
         let result = format_word(word, affix, postfix, index);
 
-        let expected = "<b>He</b>llo";
+        let expected = "[b]He[/b]llo";
         assert_eq!(expected, result);
+    }
+
+    macro_rules! test_process_word_parametrized {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (word, expected) = $value;
+                let affix = "[b]";
+                let postfix = "[/b]";
+
+                let result = process_word(word, affix, postfix);
+
+                assert_eq!(expected, result);
+            }
+        )*
+        }
+    }
+
+    test_process_word_parametrized! {
+        test_process_word_with_whitespace: (" ", " "),
+        test_process_word_with_non_word: (":", ":"),
+        test_process_word_with_number: ("1990", "1990"),
+        test_process_word_with_utf8_word: ("déjà", "[b]dé[/b]jà"),
+        test_process_word_with_word: ("Hello", "[b]He[/b]llo"),
     }
 }
