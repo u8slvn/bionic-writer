768e2fa3ed6df296d70d4c364c24372190d4cc46
u8slvn <u8slvn@gmail.com>
2024-02-09 14:48:08 +0100

    docs: update readme

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # Bionic Writer
+
+**Bio**nic **Wri**ter **i**s **a** **Pyt**hon **lib**rary **t**o **wr**ite **te**xt **wi**th **Bio**nic **Rea**ding **st**yle **f**or **a**ny **ki**nd **o**f **for**mat.
+
+## About Bionic Reading method
+
+From: [bionic-reading.com/](https://bionic-reading.com/br-method/)
+
+> Bionic Reading® revises texts so that the most concise parts of words are highlighted. This guides the eye over the text and the brain remembers previously learned words more quickly. Save precious time. Gain advantages. Learn new things faster.
+
+Does Bionic Reading really speed-up your reading speed? According to this [paper](https://blog.readwise.io/bionic-reading-results/), probably not. Whatsoever it's still a nice screen reading technique that seems to help many people to stay focused during their reading session.
+
+## Usage
+
+### Install
+
+```bash
+pip install bionic-writer
+```
+
+### Quickstart examples
+
+```python
+import bionic_writer
+
+text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+
+# Write a stylized Bionic Reading text in Markdown format.
+bionic_md = bionic_writer.write(text=text, affix="**", postfix="**")
+
+# Write a stylized Bionic Reading text in HTML format.
+bionic_html = bionic_writer.write(text=text, affix="<b>", postfix="</b>")
+
+print("Markdown:\n", bionic_md, "\n")
+print("HTML:\n", bionic_html)
+```
+
+Output:
+
+```text
+Markdown:
+**Lo**rem **ip**sum **do**lor **s**it **am**et, **conse**ctetur **adipi**scing **el**it.
+
+HTML:
+<b>Lo</b>rem <b>ip</b>sum <b>do</b>lor <b>s</b>it <b>am</b>et, <b>conse</b>ctetur <b>adipi</b>scing <b>el</b>it.
+```
--- tests/test_bionic_writer.py
+++ tests/test_bionic_writer.py
@@ -28,38 +28,3 @@ def test_bic_writer_write_with_markdown():
         "**M**r **Gl**ass."
     );
     assert expected == result
-
-# Rust version
-#
-# Move to .rs file and Add "rlib" to crate-type.
-#
-# #[test]
-# fn write_text_as_bionic_reading() {
-#     let text = concat!(
-#         "Now that we know who you are, I know who I am. I'm not ",
-#         "a mistake! It all makes sense! In a comic, you know how you can tell ",
-#         "who the arch-villain's going to be? He's the exact opposite of the ",
-#         "hero. And most times they're friends, like you and me! I should've ",
-#         "known way back when... You know why, David? Because of the kids. They ",
-#         "called me Mr Glass."
-#     );
-#     let affix = "**";
-#     let postfix = "**";
-
-#     let result = bionic_writer::write(text, affix, postfix);
-
-#     let expected = concat!(
-#         "**N**ow **th**at **w**e **kn**ow **w**ho **y**ou **a**re, **I** ",
-#         "**kn**ow **w**ho **I** **a**m. **I**'**m** **n**ot **a** ",
-#         "**mis**take! **I**t **a**ll **ma**kes **se**nse! **I**n **a** ",
-#         "**co**mic, **y**ou **kn**ow **h**ow **y**ou **c**an **te**ll **w**ho ",
-#         "**t**he **ar**ch-**vil**lain'**s** **go**ing **t**o **b**e? ",
-#         "**H**e'**s** **t**he **ex**act **oppo**site **o**f **t**he **he**ro. ",
-#         "**A**nd **mo**st **ti**mes **th**ey'**r**e **fri**ends, **li**ke ",
-#         "**y**ou **a**nd **m**e! **I** **sho**uld'**v**e **kn**own **w**ay ",
-#         "**ba**ck **wh**en... **Y**ou **kn**ow **w**hy, **Da**vid? ",
-#         "**Bec**ause **o**f **t**he **ki**ds. **Th**ey **cal**led **m**e ",
-#         "**M**r **Gl**ass."
-#     );
-#     assert_eq!(expected, result.unwrap());
-# }
